ca94f94fee5cd16b52bb7412ac90a095efa859cc
Tyron Bechayda <tyronbechayda1112@gmail.com>
2025-02-05 13:48:22 +0800

    Refactor table views for books and students to improve HTML structure and readability

--- resources/views/import/books/table.blade.php
+++ resources/views/import/books/table.blade.php
@@ -3,65 +3,29 @@
   <table class="table-fixed m-4 bg-white">
     <thead class="bg-blue-400 font-bold text-slate-200">
       <tr>
-        <th>
-          Accession
-        </th>
-        <th>
-          Call Number
-        </th>
-        <th>
-          Title
-        </th>
-        <th>
-          Edition
-        </th>
-        <th>
-          Publication
-        </th>
-        <th>
-          Publisher
-        </th>
-        <th>
-          Copyright
-        </th>
-        <th>
-          Remarks
-        </th>
-        <th>
-          Authors
-        </th>
+        <th>Accession</th>
+        <th>Call Number</th>
+        <th>Title</th>
+        <th>Edition</th>
+        <th>Publication</th>
+        <th>Publisher</th>
+        <th>Copyright</th>
+        <th>Remarks</th>
+        <th>Authors</th>
       </tr>
     </thead>
     <tbody class="text-center">
       @forelse($data as $item)
         <tr>
-          <td>
-            {{ $item['accession'] }}
-          </td>
-          <td>
-            {{ $item['call_number'] }}
-          </td>
-          <td>
-            {{ $item['title'] }}
-          </td>
-          <td>
-            {{ $item['edition'] }}
-          </td>
-          <td>
-            {{ $item['publication'] }}
-          </td>
-          <td>
-            {{ $item['publisher'] }}
-          </td>
-          <td>
-            {{ $item['copyrights'] }}
-          </td>
-          <td>
-            {{ $item['remarks'] }}
-          </td>
-          <td>
-            {{ $item['authors'] }}
-          </td>
+          <td>{{ $item['accession'] }}</td>
+          <td>{{ $item['call_number'] }}</td>
+          <td>{{ $item['title'] }}</td>
+          <td>{{ $item['edition'] }}</td>
+          <td>{{ $item['publication'] }}</td>
+          <td>{{ $item['publisher'] }}</td>
+          <td>{{ $item['copyrights'] }}</td>
+          <td>{{ $item['remarks'] }}</td>
+          <td>{{ $item['authors'] }}</td>
         </tr>
       @empty
         <tr>
--- resources/views/import/students/table.blade.php
+++ resources/views/import/students/table.blade.php
@@ -3,89 +3,37 @@
   <table class="table-fixed m-4 bg-white">
     <thead class="bg-blue-400 font-bold text-slate-200">
       <tr>
-        <th>
-          LRN
-        </th>
-        <th>
-          Employee ID
-        </th>
-        <th>
-          RFID
-        </th>
-        <th>
-          First Name
-        </th>
-        <th>
-          Middle Name
-        </th>
-        <th>
-          Last Name
-        </th>
-        <th>
-          Suffix
-        </th>
-        <th>
-          Grade
-        </th>
-        <th>
-          Section
-        </th>
-        <th>
-          Role
-        </th>
-        <th>
-          Email
-        </th>
-        <th>
-          Profile
-        </th>
-        <th>
-          Password
-        </th>
+        <th>LRN</th>
+        <th>Employee ID</th>
+        <th>RFID</th>
+        <th>First Name</th>
+        <th>Middle Name</th>
+        <th>Last Name</th>
+        <th>Suffix</th>
+        <th>Grade</th>
+        <th>Section</th>
+        <th>Role</th>
+        <th>Email</th>
+        <th>Profile</th>
+        <th>Password</th>
       </tr>
     </thead>
     <tbody class="text-center">
       @forelse($data as $item)
         <tr>
-          <td>
-            {{ $item['lrn'] }}
-          </td>
-          <td>
-            {{ $item['employee_id'] }}
-          </td>
-          <td>
-            {{ $item['rfid'] }}
-          </td>
-          <td>
-            {{ $item['first_name'] }}
-          </td>
-          <td>
-            {{ $item['middle_name'] }}
-          </td>
-          <td>
-            {{ $item['last_name'] }}
-          </td>
-          <td>
-            {{ $item['suffix'] }}
-          </td>
-          <td>
-            {{ $item['grade_level'] }}
-          </td>
-          <td>
-            {{ $item['section'] }}
-          </td>
-          <td>
-            {{ $item['role_id'] }}
-          </td>
-          <td>
-            {{ $item['email'] }}
-          </td>
-          <td>
-            {{ $item['profile_image'] }}
-          </td>
-          <td>
-            {{ $item['password'] }}
-          </td>
+          <td>{{ $item['lrn'] }}</td>
+          <td>{{ $item['employee_id'] }}</td>
+          <td>{{ $item['rfid'] }}</td>
+          <td>{{ $item['first_name'] }}</td>
+          <td>{{ $item['middle_name'] }}</td>
+          <td>{{ $item['last_name'] }}</td>
+          <td>{{ $item['suffix'] }}</td>
+          <td>{{ $item['grade_level'] }}</td>
+          <td>{{ $item['section'] }}</td>
+          <td>{{ $item['role_id'] }}</td>
+          <td>{{ $item['email'] }}</td>
+          <td>{{ $item['profile_image'] }}</td>
+          <td>{{ $item['password'] }}</td>
         </tr>
       @empty
         <tr>
